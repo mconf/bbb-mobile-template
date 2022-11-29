853949ae0c1d1d50d42993e6d4acf36411d2eda4
Max Franke <max.franke@acad.pucrs.br>
2022-11-29 14:01:00 -0300

    fix: jUrl

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ const App = () => {
   return (
     <BbbMobileSdk
       // join url 
-      jUrl={"https://live-oc002.elos.dev/bigbluebutton/api/join?fullName=User+1319332&meetingID=random-7103063&password=mp&redirect=true&checksum=fca7b978fe28fa0ead2a9c02bee974cb9d18f750"}
-      onLeaveSession={() => console.log("exited")}
+      jUrl={} onLeaveSession={() => console.log("exited")}
     />
   )
 };

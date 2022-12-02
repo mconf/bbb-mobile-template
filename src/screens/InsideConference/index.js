7210d3bceda89d2e06dd09e0211dd40aa2e79755
germanocaumo <germanocaumo@gmail.com>
2022-12-02 13:51:23 +0100

    chore: bump version and navigate to home on leave

--- src/screens/InsideConference/index.js
+++ src/screens/InsideConference/index.js
@@ -1,12 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
 import BbbMobileSdk from "bbb-mobile-sdk";
 
 const InsideConference = (props) => {
   const { route } = props;
+  const navigation = useNavigation();
   const joinUrl = route.params.joinUrl;
   return (
     <BbbMobileSdk
       // join url 
-      jUrl={joinUrl} onLeaveSession={() => console.log("exited")}
+      jUrl={joinUrl} 
+      onLeaveSession={() => navigation.replace("Home")}
     />
   )
 };

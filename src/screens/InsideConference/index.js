191561283d3e41a48790b52a4757c6fec7b950bb
germanocaumo <germanocaumo@gmail.com>
2022-12-12 18:00:24 +0000

    feat: ask for confirmation when pressing back

--- src/screens/InsideConference/index.js
+++ src/screens/InsideConference/index.js
@@ -1,10 +1,34 @@
 import { useNavigation } from "@react-navigation/native";
 import BbbMobileSdk from "bbb-mobile-sdk";
+import { useEffect } from 'react';
+import { BackHandler, Alert } from 'react-native';
 
 const InsideConference = (props) => {
   const { route } = props;
   const navigation = useNavigation();
   const joinUrl = route.params.joinUrl;
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Hold on!", "Are you sure you want to leave the meeting?", [
+        {
+          text: "Cancel",
+          onPress: () => null,
+          style: "cancel"
+        },
+        { text: "YES", onPress: () => navigation.replace("Home") }
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <BbbMobileSdk
       // join url 

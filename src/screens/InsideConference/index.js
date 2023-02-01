c4d24b3e343d85f648535517ca3bf742ee8a501d
Max Franke <max.franke@acad.pucrs.br>
2023-02-01 10:30:49 -0300

    feat(android): use v0.6.8 sdk versions
    Add android permissions

--- src/screens/InsideConference/index.js
+++ src/screens/InsideConference/index.js
@@ -1,12 +1,16 @@
 import BbbMobileSdk from "bbb-mobile-sdk";
+import { useNavigation } from "@react-navigation/native";
 
 const InsideConference = (props) => {
   const { route } = props;
   const joinUrl = route.params.joinUrl;
+  const navigation = useNavigation();
+
   return (
     <BbbMobileSdk
       // join url 
-      jUrl={joinUrl} onLeaveSession={() => console.log("exited")}
+      jUrl={joinUrl}
+      onLeaveSession={() => navigation.replace("Home")}
     />
   )
 };
